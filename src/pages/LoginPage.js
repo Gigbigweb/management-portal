17a53da2fd1c808b01bf5e8550a9e86549bf911c
Gigbigweb <web@gig-big.com>
2026-03-21 14:05:14 +0530

    update

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -70,7 +70,7 @@ export default function LoginPage() {
         <Container maxWidth="sm">
           <StyledContent>
             <Typography variant="h4" gutterBottom>
-              Sign in to Teamlans
+              Sign in to Gig-big Mangemnet
             </Typography>
             
             <LoginForm />
