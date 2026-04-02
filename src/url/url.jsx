5139fc0f78d8d994b421d6a837880edf6cb5d29c
Gigbigweb <web@gig-big.com>
2026-04-02 17:01:56 +0530

    update

--- src/url/url.jsx
+++ src/url/url.jsx
@@ -1,5 +1,5 @@
-export const Url = "http://localhost:8080" 
-// export const Url = "https://www.gig-big.in" 
+// export const Url = "http://localhost:8080" 
+export const Url = "https://www.gig-big.in" 
 // export const Url = "https://teamlans.in" 
 // export const Url = "https://rude-dolls-wonder.loca.lt" 
 
